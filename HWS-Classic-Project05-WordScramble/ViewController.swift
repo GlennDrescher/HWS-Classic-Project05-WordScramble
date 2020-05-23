8b2e557e3e67ddf4e491a095f66bd27a7bea4587
Glenn Drescher <GlennD@me.com>
2020-05-23 19:16:46 +0200

    Initial Commit

---
+++ HWS-Classic-Project05-WordScramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWS-Classic-Project05-WordScramble
+//
+//  Created by Glenn Drescher on 23/05/2020.
+//  Copyright © 2020 Glenn Drescher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
